f91419e7dba484bfa663d1755bb7c20080848b2d
Alek Slominski <aslom@us.ibm.com>
2017-05-09 15:09:52 -0400

    swift 3.1.1 whisk action and parametrized docker build

--- swift-playground-action/build.sh
+++ swift-playground-action/build.sh
@@ -1,5 +1,12 @@
 # build docker images
 echo Building base image from OpenWhisk Swift3 Action
 (cd swift3Action; docker build -t swift3action:base .)
-echo Building Swift Playground enabled version of  OpenWhisk Swift3 Action
+echo Building base image from OpenWhisk Swift3 Action
+(cd swift311Action; docker build -t swift311action:base .)
+
+echo Building Swift Playground enabled version of OpenWhisk Swift3 Action
 docker build -t swift3action:playground .
+
+echo Building Swift Playground enabled experimental version of OpenWhisk Swift3.1.1 Action
+
+docker build --build-arg BASE_IMG=swift311action:base -t swift311action:playground .
